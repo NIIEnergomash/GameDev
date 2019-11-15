fd60774be7968c381b6f4411c0b4af85523afb77
NIIEnergomash <55782742+NIIEnergomash@users.noreply.github.com>
2019-11-15 10:51:51 +0300

    Домашнее задание к уроку №6

---
+++ Lesson-6/Lesson61/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * 
+ * Усов Андрей
+ * 
+ * 3. Переделать программу Пример использования коллекций для решения следующих задач:
+    а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+    в) отсортировать список по возрасту студента;
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Student
+{
+    public string lastName;
+    public string firstName;
+    public string university;
+    public string faculty;
+    public int course;
+    public string department;
+    public int group;
+    public string city;
+
+    // добавили public, что элемент стал видимым
+    public int age;
+    // Создаем конструктор
+    public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.university = university;
+        this.faculty = faculty;
+        this.department = department;
+        this.course = course;
+        this.age = age;
+        this.group = group;
+        this.city = city;
+    }
+}
+class Program
+{
+    static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+    {
+
+        return String.Compare(st1.firstName, st2.firstName);          // Сравниваем две строки
+    }
+    static void Main(string[] args)
+    {
+        int bakalavr = 0;
+        int magistr = 0;
+        int StudentCount = 0;
+
+        List<Student> list = new List<Student>();                             // Создаем список студентов
+        DateTime dt = DateTime.Now;
+        StreamReader sr = new StreamReader("students_6.csv");
+        while (!sr.EndOfStream)
+        {
+            try
+            {
+                string[] s = sr.ReadLine().Split(';');
+                // Добавляем в список новый экземпляр класса Student
+                list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                // Одновременно подсчитываем количество бакалавров и магистров
+                if (int.Parse(s[5]) < 5) bakalavr++; else magistr++;
+                // Подсчитываем количество студентов, учащихся на 5 и 6 курсах
+                if (int.Parse(s[5]) == 5 | int.Parse(s[5]) == 6) StudentCount++;
+              
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                // Выход из Main
+                if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+            }
+        }
+        sr.Close();
+        list.Sort(new Comparison<Student>(MyDelegat));
+        Console.WriteLine("Всего студентов:" + list.Count);
+        Console.WriteLine("Магистров:{0}", magistr);
+        Console.WriteLine("Бакалавров:{0}", bakalavr);
+        foreach (var v in list) Console.WriteLine(v.firstName);
+        Console.WriteLine(DateTime.Now - dt);
+        Console.ReadKey();
+    }
+}
+
+
+
---
+++ Lesson-6/Lesson62/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Усов Андрей
+ * Изменить программу вывода таблицы функции так, чтобы можно было передавать функции
+ * типа double (double, double). Продемонстрировать работу на функции с функцией a* x^2 
+ * и функцией a* sin(x).
+ */
+
+using System;
+// Описываем делегат. В делегате описывается сигнатура методов, на
+// которые он сможет ссылаться в дальнейшем (хранить в себе)
+public delegate double Fun(double x, double a);
+
+class Program
+{
+    // Создаем метод, который принимает делегат
+    // На практике этот метод сможет принимать любой метод
+    // с такой же сигнатурой, как у делегата
+    public static void Table(Fun F, double x, double b, double a)
+    {
+        Console.WriteLine("----- X ----- Y -----");
+        while (x <= b)
+        {
+            Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x,a));
+            x += 1;
+        }
+        Console.WriteLine("---------------------");
+    }
+    // Создаем метод для передачи его в качестве параметра в Table
+    public static double MyFunc(double x, double a)
+    {
+        return a* x * x * x;
+    }
+    /// <summary>
+    /// Метод для передачи его в качестве параметра в Table для расчета фукнции a*sin(x)
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="a"></param>
+    /// <returns></returns>
+    public static double MySin(double x, double a)
+    {
+        return a * Math.Sin(x);
+    }
+
+    static void Main()
+    {
+        // Создаем новый делегат и передаем ссылку на него в метод Table
+        Console.WriteLine("Таблица функции MyFunc:");
+        // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
+        //Table(new Fun(MyFunc), -2, 2, 1);
+        Console.WriteLine("Еще раз та же таблица, но вызов организован по новому");
+        // Упрощение(c C# 2.0).Делегат создается автоматически.            
+        Table(MyFunc, -2, 2, 2);
+        Console.WriteLine("Таблица функции a*Sin:");
+        Table(MySin, -2, 2, 2);      // Можно передавать уже созданные методы
+        Console.WriteLine("Таблица функции a*x^2:");
+        // Упрощение(с C# 2.0). Использование анонимного метода
+        Table(delegate (double x, double a) { return a * x * x; }, 0, 3, 2);
+        Console.ReadLine();
+    }
+}
