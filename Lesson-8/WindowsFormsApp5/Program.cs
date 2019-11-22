58304227a11091a2ebbee7141453d0bfb42699bb
NIIEnergomash <55782742+NIIEnergomash@users.noreply.github.com>
2019-11-22 15:34:15 +0300

    Домашнее задание к уроку №8

---
+++ Lesson-8/WindowsFormsApp5/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ Усов Андрей
+1. Создать приложение, показанное на уроке
+д) Добавить пункт меню Save As, в котором можно выбрать имя для сохранения базы данных (элемент SaveFileDialog).
+
+2. Используя полученные знания и класс TrueFalse, разработать игру «Верю — не верю».
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson81
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
